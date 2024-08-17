458c06d7340e48e4854a38d8362154d3fd03ed4c
Khaledrae <educatejulius@gmail.com>
2024-08-17 13:06:21 +0000

    Kidogo ikatae

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-cosole.log('C is fun');
-cosole.log('Python is cool');
-cosole.log('JavaScript is amazing');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const arg = parseInt(args[0], 10);
+if (!isNaN(arg)) {
+  console.log(`My number: ${arg}`);
+} else {
+  console.log('Not a number');}
+} 
