20750e4c4dfa65a78798fd28c046885b5832787b
Khaledrae <educatejulius@gmail.com>
2024-08-17 00:05:40 +0000

    Sielewi mambo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-const args = process.argv().s;ice('2');
-if(args<1) console.log('No Argument');
-else if(args>1) console.log('Arguments found');
-else console.log('Argument found');
-console.log(args);
+const args = process.argv().slice('2');
+if (args < 1) {
+  console.log('No Argument');
+} else if (args>1) {
+  console.log('Arguments found');
+} else {
+ console.log('Argument found');
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let args = process.argv().slice('2');
+let arg = args[0];
+if (arg != unidentified){
+  console.log(arg);
+} else {
+  console.log('No Arguments');
+}
